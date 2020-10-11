9d600e980eb9e947c7af95dd990eb2749f63696d
Jeffrytom <jeffryabraham2014@gmail.com>
2020-10-12 00:39:52 +0530

    user authentication

--- src/main/java/com/banking/auth/controller/AuthenticationController.java
+++ src/main/java/com/banking/auth/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,11 +21,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import com.banking.auth.request.dto.LoginRequest;
 import com.banking.auth.request.dto.UserDetailsDTO;
 import com.banking.auth.response.dto.AccountDetailsDTO;
+import com.banking.auth.response.dto.ErrorResponse;
 import com.banking.auth.response.dto.JwtResponse;
 import com.banking.auth.security.UserDetailsImpl;
 import com.banking.auth.security.jwt.JwtUtils;
@@ -71,13 +74,19 @@ public class AuthenticationController {
 	
 	@RequestMapping(value= {"/create/account"},method = RequestMethod.POST)
 	@ApiOperation(value = "Create customer account")
-	public @ResponseBody ResponseEntity<AccountDetailsDTO> createUserAccount(@RequestBody UserDetailsDTO userDetailsDTO){
+	public @ResponseBody ResponseEntity<?> createUserAccount(@RequestBody UserDetailsDTO userDetailsDTO){
 		HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<UserDetailsDTO> entity = new HttpEntity<UserDetailsDTO>(userDetailsDTO,headers); 
-        AccountDetailsDTO accountDetailsDTO= restTemplate.exchange("http://localhost:8080/banking/user/create/account", HttpMethod.POST, entity,AccountDetailsDTO.class).getBody();
-        return new ResponseEntity<>(accountDetailsDTO,accountDetailsDTO.getHttpStatus());
+        HttpEntity<Object> entity = new HttpEntity<Object>(userDetailsDTO,headers); 
+	    try {    
+	        Object accountDetailsDTO= restTemplate.exchange("http://localhost:8080/banking/user/create/account", HttpMethod.POST, entity,Object.class).getBody();
+	        return new ResponseEntity<>(accountDetailsDTO,HttpStatus.OK);
+		} catch (HttpStatusCodeException ex) {
+	    	ErrorResponse errorResponse = new ErrorResponse();
+	    	errorResponse.setErrorCode(ex.getStatusCode().toString());
+        	errorResponse.setMessage(ex.getResponseBodyAsString());
+        	return new ResponseEntity<>(errorResponse,HttpStatus.valueOf(ex.getRawStatusCode()));
+		}
 	}
-	
 	
 }
--- src/main/java/com/banking/auth/controller/BankingOperationsController.java
+++ src/main/java/com/banking/auth/controller/BankingOperationsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,10 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import com.banking.auth.request.dto.CreditRequestDTO;
 import com.banking.auth.request.dto.DebitRequestDTO;
+import com.banking.auth.response.dto.ErrorResponse;
 import com.banking.auth.response.dto.UpdatedAccountDetails;
 
 import io.swagger.annotations.ApiOperation;
@@ -38,9 +41,16 @@ public class BankingOperationsController {
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization", authorization);
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<CreditRequestDTO> entity = new HttpEntity<CreditRequestDTO>(creditRequestDTO,headers); 
-        UpdatedAccountDetails updatedAccountDetails= restTemplate.exchange("http://localhost:9090/banking/operations/credit", HttpMethod.POST, entity,UpdatedAccountDetails.class).getBody();
-        return new ResponseEntity<>(updatedAccountDetails,updatedAccountDetails.getHttpStatus());
+        HttpEntity<Object> entity = new HttpEntity<Object>(creditRequestDTO,headers); 
+        try {
+        	Object updatedAccountDetails= restTemplate.exchange("http://localhost:9090/banking/operations/credit", HttpMethod.POST, entity,Object.class).getBody();
+        	return new ResponseEntity<>(updatedAccountDetails,HttpStatus.OK);
+        } catch (HttpStatusCodeException ex) {
+        	ErrorResponse errorResponse = new ErrorResponse();
+        	errorResponse.setErrorCode(ex.getStatusCode().toString());
+        	errorResponse.setMessage(ex.getResponseBodyAsString());
+        	return new ResponseEntity<>(errorResponse,HttpStatus.valueOf(ex.getRawStatusCode()));
+        }
 	}
 	
 	@RequestMapping(value= {"/debit"},method = RequestMethod.POST,
@@ -50,9 +60,16 @@ public class BankingOperationsController {
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization", authorization);
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        HttpEntity<DebitRequestDTO> entity = new HttpEntity<DebitRequestDTO>(debitRequestDTO,headers); 
-        UpdatedAccountDetails updatedAccountDetails= restTemplate.exchange("http://localhost:9090/banking/operations/debit", HttpMethod.POST, entity,UpdatedAccountDetails.class).getBody();
-        return new ResponseEntity<>(updatedAccountDetails,updatedAccountDetails.getHttpStatus());
+        HttpEntity<Object> entity = new HttpEntity<Object>(debitRequestDTO,headers); 
+        try {
+        	Object updatedAccountDetails= restTemplate.exchange("http://localhost:9090/banking/operations/debit", HttpMethod.POST, entity,Object.class).getBody();
+        	return new ResponseEntity<>(updatedAccountDetails,HttpStatus.OK);
+        } catch (HttpStatusCodeException ex) {
+        	ErrorResponse errorResponse = new ErrorResponse();
+        	errorResponse.setErrorCode(ex.getStatusCode().toString());
+        	errorResponse.setMessage(ex.getResponseBodyAsString());
+        	return new ResponseEntity<>(errorResponse,HttpStatus.valueOf(ex.getRawStatusCode()));
+        }
 	}	
 	
 }
